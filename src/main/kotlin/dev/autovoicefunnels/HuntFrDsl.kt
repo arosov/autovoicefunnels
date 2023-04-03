aa8cc4ffc200508425ead53a2196f08d45c497fa
Alexis Rosovsky <alexis@syncvr.tech>
2023-04-03 18:36:59 +0200

    HuntFrDsl: Duos before trios

--- src/main/kotlin/dev/autovoicefunnels/HuntFrDsl.kt
+++ src/main/kotlin/dev/autovoicefunnels/HuntFrDsl.kt
@@ -5,44 +5,6 @@ import dev.autovoicefunnels.models.RandomizedFrakturedNames
 import dev.autovoicefunnels.models.SimpleName
 
 fun huntFrBotDsl() = autoVoiceFunnels {
-    funnels(entryCategoryName = "Creation rch Trio") {
-        funnelDefaults {
-            disableBlacklist = true
-            disableFillUp = true
-            noTextForRoles("Mute")
-            noTextNoVoiceForRoles("Super Mute")
-            visibleForRoles("PC")
-            notVisibleForRoles("Xbox", "PlayStation")
-        }
-        transitDefaults {
-            secondsBeforeMoveToOutput = 10
-        }
-        outputDefaults {
-            outputCategoryName = "Vocaux Trio"
-            maxUsers = 3
-            tempChannelNamingStrategy = RandomizedFrakturedNames
-        }
-        funnel(entryChannelName = "Sans MMR Trio") {
-            transit(transitCategoryName = "Rch Trio") {
-                transitChannelNamingStrategy = SimpleName("Rch sans MMR")
-            }
-        }
-        funnel(entryChannelName = "MMR 3-4 Trio") {
-            transit(transitCategoryName = "Rch Trio") {
-                transitChannelNamingStrategy = SimpleName("Rch MMR 3-4")
-            }
-        }
-        funnel(entryChannelName = "MMR 4-5 Trio") {
-            transit(transitCategoryName = "Rch Trio") {
-                transitChannelNamingStrategy = SimpleName("Rch MMR 4-5")
-            }
-        }
-        funnel(entryChannelName = "MMR 5-6 Trio") {
-            transit(transitCategoryName = "Rch Trio") {
-                transitChannelNamingStrategy = SimpleName("Rch MMR 5-6")
-            }
-        }
-    }
     funnels(entryCategoryName = "Creation rch Duo") {
         funnelDefaults {
             disableBlacklist = true
@@ -81,40 +43,40 @@ fun huntFrBotDsl() = autoVoiceFunnels {
             }
         }
     }
-    funnels(entryCategoryName = "PS Creation rch Trio") {
+    funnels(entryCategoryName = "Creation rch Trio") {
         funnelDefaults {
             disableBlacklist = true
             disableFillUp = true
             noTextForRoles("Mute")
             noTextNoVoiceForRoles("Super Mute")
-            visibleForRoles("PlayStation")
-            notVisibleForRoles("Xbox", "PC")
+            visibleForRoles("PC")
+            notVisibleForRoles("Xbox", "PlayStation")
         }
         transitDefaults {
-            secondsBeforeMoveToOutput = 8
+            secondsBeforeMoveToOutput = 10
         }
         outputDefaults {
-            outputCategoryName = "PS Vocaux Trio"
+            outputCategoryName = "Vocaux Trio"
             maxUsers = 3
             tempChannelNamingStrategy = RandomizedFrakturedNames
         }
-        funnel(entryChannelName = "PS Sans MMR Trio") {
-            transit(transitCategoryName = "PS Rch Trio") {
+        funnel(entryChannelName = "Sans MMR Trio") {
+            transit(transitCategoryName = "Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch sans MMR")
             }
         }
-        funnel(entryChannelName = "PS MMR 3-4 Trio") {
-            transit(transitCategoryName = "PS Rch Trio") {
+        funnel(entryChannelName = "MMR 3-4 Trio") {
+            transit(transitCategoryName = "Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 3-4")
             }
         }
-        funnel(entryChannelName = "PS MMR 4-5 Trio") {
-            transit(transitCategoryName = "PS Rch Trio") {
+        funnel(entryChannelName = "MMR 4-5 Trio") {
+            transit(transitCategoryName = "Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 4-5")
             }
         }
-        funnel(entryChannelName = "PS MMR 5-6 Trio") {
-            transit(transitCategoryName = "PS Rch Trio") {
+        funnel(entryChannelName = "MMR 5-6 Trio") {
+            transit(transitCategoryName = "Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 5-6")
             }
         }
@@ -157,40 +119,40 @@ fun huntFrBotDsl() = autoVoiceFunnels {
             }
         }
     }
-    funnels(entryCategoryName = "Xbox Creation rch Trio") {
+    funnels(entryCategoryName = "PS Creation rch Trio") {
         funnelDefaults {
             disableBlacklist = true
             disableFillUp = true
             noTextForRoles("Mute")
             noTextNoVoiceForRoles("Super Mute")
-            visibleForRoles("Xbox")
-            notVisibleForRoles("PC", "PlayStation")
+            visibleForRoles("PlayStation")
+            notVisibleForRoles("Xbox", "PC")
         }
         transitDefaults {
             secondsBeforeMoveToOutput = 8
         }
         outputDefaults {
-            outputCategoryName = "Xbox Vocaux Trio"
+            outputCategoryName = "PS Vocaux Trio"
             maxUsers = 3
             tempChannelNamingStrategy = RandomizedFrakturedNames
         }
-        funnel(entryChannelName = "Xbox Sans MMR Trio") {
-            transit(transitCategoryName = "Xbox Rch Trio") {
+        funnel(entryChannelName = "PS Sans MMR Trio") {
+            transit(transitCategoryName = "PS Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch sans MMR")
             }
         }
-        funnel(entryChannelName = "Xbox MMR 3-4 Trio") {
-            transit(transitCategoryName = "Xbox Rch Trio") {
+        funnel(entryChannelName = "PS MMR 3-4 Trio") {
+            transit(transitCategoryName = "PS Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 3-4")
             }
         }
-        funnel(entryChannelName = "Xbox MMR 4-5 Trio") {
-            transit(transitCategoryName = "Xbox Rch Trio") {
+        funnel(entryChannelName = "PS MMR 4-5 Trio") {
+            transit(transitCategoryName = "PS Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 4-5")
             }
         }
-        funnel(entryChannelName = "Xbox MMR 5-6 Trio") {
-            transit(transitCategoryName = "Xbox Rch Trio") {
+        funnel(entryChannelName = "PS MMR 5-6 Trio") {
+            transit(transitCategoryName = "PS Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 5-6")
             }
         }
@@ -229,6 +191,44 @@ fun huntFrBotDsl() = autoVoiceFunnels {
         }
         funnel(entryChannelName = "Xbox MMR 5-6 Duo") {
             transit(transitCategoryName = "Xbox Rch Duo") {
+                transitChannelNamingStrategy = SimpleName("Rch MMR 5-6")
+            }
+        }
+    }
+    funnels(entryCategoryName = "Xbox Creation rch Trio") {
+        funnelDefaults {
+            disableBlacklist = true
+            disableFillUp = true
+            noTextForRoles("Mute")
+            noTextNoVoiceForRoles("Super Mute")
+            visibleForRoles("Xbox")
+            notVisibleForRoles("PC", "PlayStation")
+        }
+        transitDefaults {
+            secondsBeforeMoveToOutput = 8
+        }
+        outputDefaults {
+            outputCategoryName = "Xbox Vocaux Trio"
+            maxUsers = 3
+            tempChannelNamingStrategy = RandomizedFrakturedNames
+        }
+        funnel(entryChannelName = "Xbox Sans MMR Trio") {
+            transit(transitCategoryName = "Xbox Rch Trio") {
+                transitChannelNamingStrategy = SimpleName("Rch sans MMR")
+            }
+        }
+        funnel(entryChannelName = "Xbox MMR 3-4 Trio") {
+            transit(transitCategoryName = "Xbox Rch Trio") {
+                transitChannelNamingStrategy = SimpleName("Rch MMR 3-4")
+            }
+        }
+        funnel(entryChannelName = "Xbox MMR 4-5 Trio") {
+            transit(transitCategoryName = "Xbox Rch Trio") {
+                transitChannelNamingStrategy = SimpleName("Rch MMR 4-5")
+            }
+        }
+        funnel(entryChannelName = "Xbox MMR 5-6 Trio") {
+            transit(transitCategoryName = "Xbox Rch Trio") {
                 transitChannelNamingStrategy = SimpleName("Rch MMR 5-6")
             }
         }
